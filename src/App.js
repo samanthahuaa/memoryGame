8c9dda83819ea9264eec30491cc839e95be1ead5
samanthahuaa <shua30@stuy.edu>
2024-06-21 23:24:47 -0400

    permFlipped now works!

--- src/App.js
+++ src/App.js
@@ -8,50 +8,44 @@ function App() {
   const [tiles, setTiles] = useState([{value: 1, status: null}, {value: 1, status: null}, {value: 2, status: null}, {value: 2, status: null}, {value: 3, status: null}, {value: 3, status: null}]);
 
   function tileOnClick(id, status, value) {
-    console.log("clicked");
-    console.log(status);
-    console.log(value);
+    
     if (status !== "permFlipped") {
-      let tempTiles = structuredClone(tiles);
       if (status === "flipped") {
+        let tempTiles = structuredClone(tiles);
         tempTiles[id].status = null
         setfirstCard(null);
+        setTiles(tempTiles);
       } else {
-        tempTiles[id].status = "flipped";
         checkMatch(id, value);
       }
-      setTiles(tempTiles);
     }
-    console.log(status);
-    console.log("done")
   }
   
   function checkMatch(id, inputValue) {
+    let tempTiles = structuredClone(tiles);
+
     if (firstCard == null) {
       setfirstCard({value: inputValue, id: id});
+      tempTiles[id].status = "flipped";
     } else {
-      let tempTiles = structuredClone(tiles);
       if (firstCard.value === inputValue && id !== firstCard.id) {
-        setScore(score + 1);
         for (let i=0; i<tempTiles.length; i++) {
           if (tempTiles[i].value === inputValue) {
             tempTiles[i].status = "permFlipped";
           }
-// ALERT - need to make sure the order of the array stays the same so it renders on the screen in the same orientation; don't want to modify the state var, want to just copy the data into the temp and set the state at the end. no solution yet, must think
         }
-        // tempTiles = tiles.filter((value, status) => value === inputValue).map((value, status) => status = 'permFlipped');
-        // tempTiles.concat(tiles.filter((value, status) => value !== inputValue))
+        setScore(score + 1);
       } else {
         for (let i=0; i<tempTiles.length; i++) {
           if (tempTiles[i].status !== "permFlipped") {
             tempTiles[i].status = "";
           }
         }
-        // tempTiles = tiles.filter((value, status) => status !== 'permFlipped').map((value, status) => status = null);
       }
-      setTiles(tempTiles);
+      
       setfirstCard(null);
     }
+    setTiles(tempTiles);
   }
 
   return (
